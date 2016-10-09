f78c6954d4a8d862971d9f60a48602d991909247
dominikus1993 <dominikus1910@gmail.com>
2016-10-09 17:52:49 +0200

    reformat

--- src/user/reducers.ts
+++ src/user/reducers.ts
@@ -1,5 +1,5 @@
 import {AppModel} from "../global/model";
 
-export function userReducer(state: AppModel = {user : {}}, action: any){
+export function userReducer(state: AppModel = {user: {}}, action: any) {
     return state;
 }

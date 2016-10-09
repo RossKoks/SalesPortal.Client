ffe9f00aba8507273200f6d6f14cdc60ebda4da3
dominikus1993 <dominikus1910@gmail.com>
2016-10-09 19:57:39 +0200

    small change

--- src/server.tsx
+++ src/server.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
+import * as express from "express";
 import { Provider } from 'react-redux';
 import { createMemoryHistory, match } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+    const webpack = require("webpack");
+    const webpackConfig = require("../webpack.config.js");
+    const webpackCompiler = webpack(webpackConfig);
+}
